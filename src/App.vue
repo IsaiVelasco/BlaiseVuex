61ea5f34822804e5a409f7a49fc2c6e233893bfe
Isaí Velasco Hernández <velascohernandes.2017@gmail.com>
2022-03-22 23:42:19 -0600

    Inicio de correción de Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,69 +1,53 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-custom fixed-top navbar-light">
-    <div class="container px-4 px-lg-5">
-      <router-link to="/" class="navbar-brand"
-        ><strong> BLAISE CAKE</strong></router-link>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
-          <li class="nav-item">
-            <router-link to="/" class="nav-link active">Home</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link to="/about" class="nav-link">About</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link to="/ordenar" class="nav-link">Ordenar</router-link>
-          </li>
+<header id="header" class="header">
+            <div class="container">
+                <!-- logo -->
+                <div class="site-logo">
+                    <a href="index.html" class="site-logo__link">
+                        <picture>
+                            <source media="(max-width: 767px)" srcset="images/logo-mob.png">
+                            <source srcset="images/logo.png">
+                            <img class="site-logo__img" src="images/menu-logo.png" alt="logo" />
+                        </picture>
+                    </a>
+                </div>
+                <div class="header-mobile">
+                    <div class="header-nav">
+                        <div class="nav-toggle"><span class="nav-toggle__link"></span><span
+                                class="nav-toggle__link"></span><span class="nav-toggle__link"></span></div>
+                        <div class="header-nav__container">
+                            <div class="header-nav__menu">
+                                <nav class="nav">
+                                    <ul class="nav__list">
+                                        <li class="nav__item"><a href="#categorias" class="nav__link">Categorías</a>
+                                        </li>
+                                        <li class="nav__item"><a href="#tutorial" class="nav__link">Tutorial</a></li>
 
-          <li class="nav-item dropdown">
-            <a
-              class="nav-link dropdown-toggle"
-              id="navbarDropdown"
-              href="#"
-              role="button"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-              >Shop</a
-            >
-            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <li><a class="dropdown-item" href="#!">Productos</a></li>
-              <li><hr class="dropdown-divider" /></li>
-              <li>
-                <a class="dropdown-item" href="armado-blaise.html">Armado</a>
-              </li>
-              <li><a class="dropdown-item" href="#!">Novedades</a></li>
-            </ul>
-          </li>
-        </ul>
-        <form class="d-flex">
-          <button class="btn btn-outline-dark" type="submit">
-            <i class="bi-cart-fill me-1"></i>
-            Cart
-            <span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
-          </button>
-        </form>
-      </div>
-    </div>
+                                        <li class="nav__item"><a href="#pasteles" class="nav__link">Pasteles</a></li>
+                                        <li class="nav__item"><a href="#reviews" class="nav__link">Contacto</a></li>
+                                        <li class="nav__item"><a href="admin.html" class="nav__link">Admin</a></li>
+                                    </ul><!-- //nav__list -->
+                                </nav><!-- //nav -->
+                            </div>
+                            <div class="header-nav__info">
+                                <a href="tel:9999912345678">+52 546 254 9875</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div><!-- //container -->
+        </header><!-- //site-header -->
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> | 
+    <router-link to="/Ordenar">Ordenar</router-link>
   </nav>
-
-  <router-view />
+  <router-view/>
 </template>
 
 <style>
-@import url("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css");
-@import "./assets/style.css";
-@import "./assets/styles.css";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -72,5 +56,16 @@
   color: #2c3e50;
 }
 
+nav {
+  padding: 30px;
+}
 
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
 </style>
