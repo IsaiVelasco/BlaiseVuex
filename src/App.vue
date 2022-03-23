63184994c1681cb7c2a9d7481dd244e6eb66e144
Isaí Velasco Hernández <velascohernandes.2017@gmail.com>
2022-03-23 00:35:37 -0600

    Añadiendo nuevos estilos

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,78 @@
 <template>
-<header id="header" class="header">
-            <div class="container">
-                <!-- logo -->
-                <div class="site-logo">
-                    <a href="index.html" class="site-logo__link">
-                        <picture>
-                            <source media="(max-width: 767px)" srcset="images/logo-mob.png">
-                            <source srcset="images/logo.png">
-                            <img class="site-logo__img" src="images/menu-logo.png" alt="logo" />
-                        </picture>
-                    </a>
-                </div>
-                <div class="header-mobile">
-                    <div class="header-nav">
-                        <div class="nav-toggle"><span class="nav-toggle__link"></span><span
-                                class="nav-toggle__link"></span><span class="nav-toggle__link"></span></div>
-                        <div class="header-nav__container">
-                            <div class="header-nav__menu">
-                                <nav class="nav">
-                                    <ul class="nav__list">
-                                        <li class="nav__item"><a href="#categorias" class="nav__link">Categorías</a>
-                                        </li>
-                                        <li class="nav__item"><a href="#tutorial" class="nav__link">Tutorial</a></li>
+  <header id="header" class="header">
+    <div class="container">
+      <!-- logo -->
+      <div class="site-logo">
+        <a href="index.html" class="site-logo__link">
+          <picture>
+            <source media="(max-width: 767px)" srcset="images/logo-mob.png" />
+            <source srcset="images/logo.png" />
+            <img class="site-logo__img" src="images/menu-logo.png" alt="logo" />
+          </picture>
+        </a>
+      </div>
+      <div class="header-mobile">
+        <div class="header-nav">
+          <div class="nav-toggle">
+            <span class="nav-toggle__link"></span
+            ><span class="nav-toggle__link"></span
+            ><span class="nav-toggle__link"></span>
+          </div>
+          <div class="header-nav__container">
+            <div class="header-nav__menu">
+              <nav class="nav">
+                <ul class="nav__list">
+                  <li class="nav__item">
+                    <router-link to="/" class="nav__link">Home</router-link>
+                  </li>
+                  <li class="nav__item">
+                    <router-link to="/about" class="nav__link" >About</router-link>
+                  </li>
+                  <li class="nav__item">
+                    <router-link to="/Ordenar" class="nav__link">Ordenar</router-link>
+                  </li>
+                  <li class="nav__item">
+                    <a href="#categorias" class="nav__link">Categorías</a>
+                  </li>
+                  <li class="nav__item">
+                    <a href="#tutorial" class="nav__link">Tutorial</a>
+                  </li>
 
-                                        <li class="nav__item"><a href="#pasteles" class="nav__link">Pasteles</a></li>
-                                        <li class="nav__item"><a href="#reviews" class="nav__link">Contacto</a></li>
-                                        <li class="nav__item"><a href="admin.html" class="nav__link">Admin</a></li>
-                                    </ul><!-- //nav__list -->
-                                </nav><!-- //nav -->
-                            </div>
-                            <div class="header-nav__info">
-                                <a href="tel:9999912345678">+52 546 254 9875</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                  <li class="nav__item">
+                    <a href="#pasteles" class="nav__link">Pasteles</a>
+                  </li>
+                  <li class="nav__item">
+                    <a href="#reviews" class="nav__link">Contacto</a>
+                  </li>
+                  <li class="nav__item">
+                    <a href="admin.html" class="nav__link">Admin</a>
+                  </li>
+                </ul>
+                <!-- //nav__list -->
+              </nav>
+              <!-- //nav -->
+            </div>
+            <div class="header-nav__info">
+              <a href="tel:9999912345678">+52 546 254 9875</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- //container -->
+  </header>
+  <!-- //site-header -->
 
-
-            </div><!-- //container -->
-        </header><!-- //site-header -->
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> | 
-    <router-link to="/Ordenar">Ordenar</router-link>
-  </nav>
-  <router-view/>
+  <router-view />
 </template>
 
+
 <style>
+@import "./css/libs.min.css";
+@import "./css/main.css";
+@import url("https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css");
+@import url("https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@400;600;700&display=swap");
+@import "./css/main.css";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -56,16 +81,5 @@
   color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
-}
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
