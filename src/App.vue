c704ec7a0d5399c0665629ec82ae25407ecb5f23
Isaí Velasco Hernández <velascohernandes.2017@gmail.com>
2022-03-22 23:32:19 -0600

    Inicio de Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,69 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> | 
-    <router-link to="/Ordenar">Ordenar</router-link>
+  <nav class="navbar navbar-expand-lg navbar-custom fixed-top navbar-light">
+    <div class="container px-4 px-lg-5">
+      <router-link to="/" class="navbar-brand"
+        ><strong> BLAISE CAKE</strong></router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+          <li class="nav-item">
+            <router-link to="/" class="nav-link active">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/about" class="nav-link">About</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/ordenar" class="nav-link">Ordenar</router-link>
+          </li>
+
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              id="navbarDropdown"
+              href="#"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+              >Shop</a
+            >
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="#!">Productos</a></li>
+              <li><hr class="dropdown-divider" /></li>
+              <li>
+                <a class="dropdown-item" href="armado-blaise.html">Armado</a>
+              </li>
+              <li><a class="dropdown-item" href="#!">Novedades</a></li>
+            </ul>
+          </li>
+        </ul>
+        <form class="d-flex">
+          <button class="btn btn-outline-dark" type="submit">
+            <i class="bi-cart-fill me-1"></i>
+            Cart
+            <span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
+          </button>
+        </form>
+      </div>
+    </div>
   </nav>
-  <router-view/>
+
+  <router-view />
 </template>
 
 <style>
+@import url("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css");
+@import "./assets/style.css";
+@import "./assets/styles.css";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/views/OrdenarView.vue
+++ src/views/OrdenarView.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="about">
+      <br>
+      <br>
+      <br>
+      <br>
+      
     <h1>Ordenar</h1>
   </div>
 </template>
