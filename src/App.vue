da60baafb0b98bb60a23fab6495ebc2cf674550f
Isaí Velasco Hernández <velascohernandes.2017@gmail.com>
2022-03-24 00:42:55 -0600

    Corrección de links fase 2

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,8 @@
       <div class="site-logo">
         <a href="index.html" class="site-logo__link">
           <picture>
-            <source media="(max-width: 767px)" srcset="images/logo-mob.png" />
-            <source srcset="images/logo.png" />
+            <source media="(max-width: 767px)" srcset="./images/logo-mob.png" />
+            <source srcset="./images/logo.png" />
             <img class="site-logo__img" src="images/menu-logo.png" alt="logo" />
           </picture>
         </a>
@@ -44,53 +44,32 @@
                   <li class="nav__item">
                     <a href="#reviews" class="nav__link">Contacto</a>
                   </li>
-                  <li class="nav__item">
-                    <a href="admin.html" class="nav__link">Admin</a>
-                  </li>
+                  
                 </ul>
                 <!-- //nav__list -->
               </nav>
               <!-- //nav -->
             </div>
-            <div class="header-nav__info">
-              <a href="tel:9999912345678">+52 546 254 9875</a>
-            </div>
+            
           </div>
         </div>
       </div>
     </div>
     <!-- //container -->
   </header>
-  <!-- //site-header -->
+  <!-- //site-header ddd -->
 
   <router-view />
 </template>
 
 
+
 <style>
+
 @import "./css/libs.min.css";
 @import "./css/main.css";
-@import url("https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css");
 @import url("https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@400;600;700&display=swap");
+
 @import "./css/main.css";
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
