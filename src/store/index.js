f5275ea3b652229b29d34da5355a1c892ec384b7
Isaí Velasco Hernández <velascohernandes.2017@gmail.com>
2022-03-28 23:15:33 -0600

    Metodos store

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,15 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    Queso: 10,
+    Chocolate: 10,
+    Vainilla: 11,
+    TresLeches: 12,
+    Batido:10,
+    Velas:10,
+    Sarzamora:10,
+    Fresas: 10,
+    Pedidos: []
   },
   getters: {
   },
--- src/views/OrdenarView.vue
+++ src/views/OrdenarView.vue
@@ -1,49 +1,56 @@
 <template>
   <main id="main" class="main">
-        
-        <section class="section">            
-            <div class="container">
-                
-                <div class="row">
-                    
-                    <div class="col col_7 col_desktop-12 order-desktop-2">
-                        <div class="photo-list main-photo">
-                            <FondoSgv/>
-                            
-                            <form action="index.html" method="POST">
-                                
-                                <Sabores/>
-                                <Adornos/>
-                                <Formulario/>
-                                <br>
-                                <div class="center-swit">
-                                    <input  type="submit" class="btn primary-button btn-lg" value="ORDENAR"/>
-                                </div>
-                                
-                            </form>
+    <section class="section">
+      <div class="container">
+        <div class="row">
+          <div class="col col_7 col_desktop-12 order-desktop-2">
+            <div class="photo-list main-photo">
+              <FondoSgv />
 
-                        </div>
-                    </div>
-                    
+              <form @submit.prevent="agregarPedido">
+                <Sabores />
+                <Adornos />
+                <Formulario />
+                <br />
+                <div class="center-swit">
+                  <input
+                    type="submit"
+                    class="btn primary-button btn-lg"
+                    value="ORDENAR"
+                  />
                 </div>
+              </form>
             </div>
-        </section>
-        
-    </main><!-- //middle -->
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+  <!-- //middle -->
 </template>
 <script>
 // @ is an alias to /src
-import Sabores from '@/components/Sabores.vue'
-import Adornos from '@/components/Adornos.vue'
-import Formulario from '@/components/Formulario.vue'
+import Sabores from "@/components/Sabores.vue";
+import Adornos from "@/components/Adornos.vue";
+import Formulario from "@/components/Formulario.vue";
 import FondoSgv from "@/components/FondoSgv.vue";
 export default {
-  name: 'OrdenarView',
+  name: "OrdenarView",
   components: {
     Sabores,
     Adornos,
     Formulario,
-    FondoSgv
-  }
-}
+    FondoSgv,
+  },
+  methods: {
+    agregarPedido() {
+      var pedido = {
+        sabores: "",
+        adornos: "",
+        total: 0,
+        datos: "",
+      };
+    },
+  },
+};
 </script>
